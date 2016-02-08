8d8ddb8aa8e191ffaf5efd32944ecf39dcd5c63b
Sierra Morgan <sierramorgan07@gmail.com>
2016-02-08 02:17:24 -0800

    subtle update

--- js/script.js
+++ js/script.js
@@ -1,7 +1,25 @@
-$(document).on('ready', function(){
-	$('.pol').each(function(i, obj) {
-		$(obj).on('mouseover', function(){
-			
-		});
-	});
-});	
+// $(document).on('ready', function(){
+// 	$('.pol').each(function(i, obj) {
+// 		$(obj).on('click', function(){
+// 			 $('.info').show(); 
+//     	});
+//     	$('.exit').on('click', function(){
+//     		$('.info').hide();
+//     	})
+// 	});
+// 	    if ( $(this).is(':visible') ) {
+//     		$('.pol').off();
+//     	}
+//     	else {
+//     		//do nada
+//     	}	
+// });	
+$(function() {
+    $( ".draggable" ).draggable();
+  });
+
+
+
+
+
+
