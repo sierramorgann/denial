6a43ba1b47ac2db88eebd08f0c5f15b865ebc04e
Sierra Morgan <sierramorgan07@gmail.com>
2016-02-18 09:51:38 -0800

    whoa

--- js/script.js
+++ js/script.js
@@ -1,25 +1,44 @@
-// $(document).on('ready', function(){
-// 	$('.pol').each(function(i, obj) {
-// 		$(obj).on('click', function(){
-// 			 $('.info').show(); 
-//     	});
-//     	$('.exit').on('click', function(){
-//     		$('.info').hide();
-//     	})
-// 	});
-// 	    if ( $(this).is(':visible') ) {
-//     		$('.pol').off();
-//     	}
-//     	else {
-//     		//do nada
-//     	}	
-// });	
-$(function() {
-    $( ".draggable" ).draggable();
-  });
+$(document).ready(function(){
+    $(function() {
+        var a = 3;
+            $('.draggable').draggable({
+                start: function(event, ui) { $(this).css("z-index", a++); }
+            });
+        $('#dragZone div').click(function() {
+            $(this).addClass('top').removeClass('bottom');
+            $(this).siblings().removeClass('top').addClass('bottom');
+            $(this).css("z-index", a++);
+        });    
+    });
 
+    $(function() {
+        // Change this selector to find whatever your 'boxes' are
+        var boxes = $(".draggable");
 
+        // Set up click handlers for each box
+        boxes.click(function() {
+            var el = $(this), // The box that was clicked
+                max = 0;
 
+            // Find the highest z-index
+            boxes.each(function() {
+                // Find the current z-index value
+                var z = parseInt( $( this ).css( "z-index" ), 10 );
+                // Keep either the current max, or the current z-index, whichever is higher
+                max = Math.max( max, z );
+            });
 
+            // Set the box that was clicked to the highest z-index plus one
+            el.css("z-index", max + 1 );
+        });
+    });
+
+    $(function getQuote() {
+            var txt = $('#input1').val();
+                $('#output').html(txt);
+
+        $('#submit').on('click', getQuote());  
+    });      
+});
 
 
